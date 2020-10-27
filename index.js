cb623b4206da8c4b7db4cce1b5bd7e0aa4fd4984
dustinpfister <dustin.pfister@gmail.com>
2020-10-27 11:27:54 -0400

    added ejs support

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 #!/usr/bin/env node
 // using exec
-let exec = require('child_process').exec;
+let exec = require('child_process').exec,
+path = require('path');
 // getting a post and appURL string
 let port = process.argv[2] || 8000,
 appURL = 'http://localhost:' + port;
 // start express app
 let express = require('express'),
 app = express();
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 // root path
 app.get('/', (req, res) => {
-   res.send('foo');
+    //res.send('foo');
+    res.render('index',{});
 });
 // start express app on port and start chrome child process
 app.listen(port, function () {
     console.log('nodejs express app is up on port: ' + port);
     // start chrome in app mode
-    let com = 'chromium-browser --app=' + appURL + ' --new-window';
+    let com = 'chromium-browser --app=' + appURL + ' --new-window --incognito';
     console.log('starting chrome in app mode');
     console.log(com);
     // the chrome child
@@ -24,6 +29,9 @@ app.listen(port, function () {
     });
     chromeChild.stdout.on('data', (data) => {
         console.log(data);
+    });
+    chromeChild.on('message', (data) => {
+        console.log('message');
     });
     // on exit of chrome child kill this process
     chromeChild.on('exit', () => {
