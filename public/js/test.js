37f3c8d0b00837e484ad0b2d3ba776d29ee876a4
dustinpfister <dustin.pfister@gmail.com>
2020-10-27 11:53:26 -0400

    added axios

--- public/js/test.js
+++ public/js/test.js
@@ -1,3 +1,13 @@
 var el = document.createElement('p');
 el.innerText = 'javaScript working';
 document.body.appendChild(el);
+
+axios.post('/', {
+    action: 'ping'
+  })
+  .then(function (response) {
+    
+  })
+  .catch(function (error) {
+    
+  });
